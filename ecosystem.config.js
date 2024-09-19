2fbb9e22cec5301391681572db4ca7b757dcfa15
Vladimir Sulovic <sulovic@gmail.com>
2024-09-19 14:14:44 +0200

    PM2 script added

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: 'devtracker-backend',
+        script: './server.js',
+        instances: 'max',
+        exec_mode: 'cluster',
+        env: {
+          NODE_ENV: 'development',
+        },
+        env_production: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
